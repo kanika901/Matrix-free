0ea62d7ebd9fef2d5a4817e7459af2678efbdc3a
Kanika Sood <kanika901@gmail.com>
2018-03-23 12:29:56 -0700

    Compute properties for non-UFlorida matrices

---
+++ StructuralProperties/RS1_test_set2.m
@@ -0,0 +1,66 @@
+% Computes RS1 structural properties of a square matrix.
+% test set for ML classification
+% Output: nnz, max. no. of non zeros per row etc. 
+% Date: March 21, 2018
+
+cd '/Volumes/Kank/MatrixFree'
+file_location = '/Volumes/Kank/MatrixFree/TestMatrices';
+
+all_files = dir(file_location);
+all_names = { all_files.name };
+global in_file;
+
+out_file = fopen('/Volumes/Kank/MatrixFree/RS1test_set2.csv','a') ;
+fprintf(out_file,'Matrix name, MinNonzerosPerRow, NonZeroPatternSymmetryV1, lowerBandwidth, InfinityNorm, ColumnVariance, DiagonalNonZeros, DiagonalAverage \n');
+
+for j = 4: length(all_names)
+    in_file = strcat(file_location,'/', all_files(j).name);
+    [path, mat_name, ext] = fileparts(in_file);
+    A = load(in_file);
+    matname = strsplit(mat_name, ',');
+    mat_name = matname{:};
+    A = getfield(A,mat_name);
+    sprintf('Computing RS1 structural properties for: %s', mat_name)
+    
+    % 1. MinNonzerosPerRow
+    nnz_per_row = sum(A ~= 0, 2);
+    min_non_zeros_per_row = full(min(nnz_per_row));
+    sprintf('Minimum no. of non zeros per row: %d', min_non_zeros_per_row)
+
+    % 2. NonZeroPatternSymmetryV1 (Checks the nonzero pattern symmetry, if symmetric returns 1) 
+    % if A and A(Transpose) have same nonzero pattern then returns 1)
+    non_zero_pattern_A = A ~= 0;
+    non_zero_pattern_A_tran = A' ~= 0;
+    non_zero_pattern_symmetryV1 = isequal(non_zero_pattern_A, non_zero_pattern_A_tran);
+    sprintf('Non zero pattern symmetry: %d', non_zero_pattern_symmetryV1)
+
+    % 3. InfinityNorm (maximum of the absolute row sums)
+    inf_norm = norm(A,'inf'); 
+    sprintf('Infinity Norm: %d', full(inf_norm))
+
+    % 4. ColumnVariance
+    out = var(A.').';
+    [out_rows, out_cols] = size(out);
+    for i = 1: out_cols
+            col_variance = out(i) + i;
+    end
+    col_variance = col_variance / out_cols;
+    sprintf('Column variance %d', col_variance)
+
+    % 5. lowerBandwidth
+    lower_bw = bandwidth(A, 'lower');
+    sprintf('Lower bandwidth %d', lower_bw) 
+
+    % 6. DiagonalNonZeros
+    % counts the number of nonzeros on the diagonal
+    D = diag(A);
+    nnz_diag = nnz(D);
+    sprintf('Diagonal non zeros: %d', nnz_diag)
+    
+    % 7. DiagonalAverage
+    % Computes the average of the absolute values of the diagonal elements of a matrix
+    diag_avg = mean(abs(diag(A)));
+    sprintf('Diagonal average: %d', full(diag_avg))
+    
+    fprintf(out_file, '%s, %f, %f, %f, %f, %f, %f, %f \n', mat_name, min_non_zeros_per_row, non_zero_pattern_symmetryV1, lower_bw, full(inf_norm),col_variance, nnz_diag, full(diag_avg) );
+end
